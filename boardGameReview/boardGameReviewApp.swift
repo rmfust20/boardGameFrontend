146e45d94f6d04d2b764311dece2721cc6905bcc
rmfust20 <robertmfusting@gmail.com>
2025-12-06 13:03:09 -0500

    Initial Commit

---
+++ boardGameReview/boardGameReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  boardGameReviewApp.swift
+//  boardGameReview
+//
+//  Created by Robert Fusting on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct boardGameReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
